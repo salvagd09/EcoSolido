27af35ca8fd760e7b4190643ad84d989c91de5bb
salvagd09 <vasco14_100@hotmail.com>
2026-05-23 13:16:50 -0500

    Entidades hechas

--- Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Models/Entities/IncidenciaFotoEntity.java
+++ Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Models/Entities/IncidenciaFotoEntity.java
@@ -1,4 +1,49 @@
 package com.DisenoProductos.EcoSolido.Models.Entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="incidenciaFoto")
 public class IncidenciaFotoEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id_foto")
+    private Integer idFoto;
+    /*
+    * Para que se conecte correctamente con cloudinary*/
+    @Column(name="public_id")
+    private Integer publicId;
+    @Column(name="url_foto")
+    private String urlFoto;
+    @ManyToOne
+    @JoinColumn(name="id_incidencia")
+    private IncidenciaEntity incidencia;
+    public Integer getIdFoto() {
+        return idFoto;
+    }
+    public void setIdFoto(Integer idFoto) {
+        this.idFoto = idFoto;
+    }
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
+
+    public Integer getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(Integer publicId) {
+        this.publicId = publicId;
+    }
+
+    public IncidenciaEntity getIncidencia() {
+        return incidencia;
+    }
+
+    public void setIncidencia(IncidenciaEntity incidencia) {
+        this.incidencia = incidencia;
+    }
 }
--- Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Services/IncidenciaService.java
+++ Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Services/IncidenciaService.java
@@ -1,4 +1,11 @@
 package com.DisenoProductos.EcoSolido.Services;
 
-public class IncidenciaService {
+import com.DisenoProductos.EcoSolido.Models.Entities.IncidenciaEntity;
+import com.DisenoProductos.EcoSolido.Repositories.IncidenciaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IncidenciaService  {
+    @Autowired
+    public IncidenciaRepository incidenciaRepository;
+
 }
