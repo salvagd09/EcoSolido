2db62a2037c015c08709718e2b146c2766fced30
salvagd09 <vasco14_100@hotmail.com>
2026-05-24 11:55:21 -0500

    Mejora del BackupScheduler

--- Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Services/BackupScheduler.java
+++ Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Services/BackupScheduler.java
@@ -1,16 +1,13 @@
 package com.DisenoProductos.EcoSolido.Services;
 
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDate;
 
 @Component
     public class BackupScheduler {
-        @Autowired
-        private EntityManager entityManager;
         @Scheduled(cron = "0 0 0 * * *") // Cada día a medianoche
         public void ejecutarBackup() {
             try {
