5e84f257ddd79b97533103718957df4a12c6e0fc
salvagd09 <vasco14_100@hotmail.com>
2026-06-06 12:46:06 -0500

    Cambios en el Backend hechos por Itzair

--- Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Middleware/CorsConfig.java
+++ Backend/EcoSolido/src/main/java/com/DisenoProductos/EcoSolido/Middleware/CorsConfig.java
@@ -11,8 +11,9 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://localhost:5174",
+                "http://127.0.0.1:5173",
+                "http://127.0.0.1:5174"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
